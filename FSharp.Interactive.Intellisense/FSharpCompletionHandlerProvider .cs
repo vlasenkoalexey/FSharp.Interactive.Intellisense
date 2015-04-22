2b259ab4b3f948055db6507d470811c9f815e1e7
Aleksey Vlasenko <vlasenkoalexey@gmail.com>
2015-04-22 02:57:24 -0700

    Original commit, autocomplete popup shows in F# interactive

---
+++ FSharp.Interactive.Intellisense/FSharpCompletionHandlerProvider .cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSharp.Interactive.Intellisense
+{
+    [Export(typeof(IVsTextViewCreationListener))]
+    [Name("token completion")]
+    [ContentType("any")]
+    [TextViewRole(PredefinedTextViewRoles.Analyzable)]
+    internal class FSharpCompletionHandlerProvider : IVsTextViewCreationListener
+    {
+        [Import]
+        internal IVsEditorAdaptersFactoryService AdapterService = null;
+        [Import]
+        internal ICompletionBroker CompletionBroker { get; set; }
+        [Import]
+        internal SVsServiceProvider ServiceProvider { get; set; }
+
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            ITextView textView = AdapterService.GetWpfTextView(textViewAdapter);
+            if (textView == null)
+                return;
+
+            Func<FSharpCompletionCommandHandler> createCommandHandler = delegate() { return new FSharpCompletionCommandHandler(textViewAdapter, textView, this); };
+            textView.Properties.GetOrCreateSingletonProperty(createCommandHandler);
+        }
+
+    }
+}
---
+++ FSharp.Interactive.Intellisense/FSharpCompletionSourceProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSharp.Interactive.Intellisense
+{
+    [Export(typeof(ICompletionSourceProvider))]
+    [ContentType("any")]
+    [Name("token completion handler")]
+    internal class FSharpCompletionSourceProvider : ICompletionSourceProvider
+    {
+
+        [Import]
+        internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
+
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer)
+        {
+            return new FSharpCompletionSource(this, textBuffer);
+        }
+
+    }
+}

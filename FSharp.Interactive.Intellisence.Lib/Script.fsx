c3c1e4ca4cd598a5e519693e3c9b0562e3ebf815
Aleksey Vlasenko <vlasenkoalexey@gmail.com>
2015-04-30 00:52:29 -0700

    - Script to autocomplete interactive session variable names and types from loaded assemblies

--- FSharp.Interactive.Intellisence.Lib/Script.fsx
+++ FSharp.Interactive.Intellisence.Lib/Script.fsx
@@ -33,22 +33,95 @@ let removePropertyPrefix (memberName:String) =
     else
         memberName
 
+let fsiAssembly = 
+    System.AppDomain.CurrentDomain.GetAssemblies() 
+    |> Seq.find (fun assm -> assm.GetName().Name = "FSI-ASSEMBLY")
+
+let fAssembly = 
+    System.AppDomain.CurrentDomain.GetAssemblies() 
+    |> Seq.find (fun assm -> assm.GetName().Name = "Fsi")
+
+System.AppDomain.CurrentDomain.GetAssemblies()
+|> Seq.map (fun assm -> assm.GetName().Name)
+|> Seq.toList
+
+fsiAssembly.GetReferencedAssemblies()
+|> Seq.map (fun assm -> assm.FullName)
+////|> Seq.filter(fun n -> n.ToLowerInvariant().Contains("dll"))
+|> Seq.toList
+
+#r @"C:\Projects\TorrentRT\packages\FSharp.Data.2.1.1\lib\net40\FSharp.Data.dll";;
+open FSharp.Data;
+
+type Rss = XmlProvider<"https://www.torrentz.com/feed?f=trailer">
+let feed = Rss.Load("https://www.torrentz.com/feed?f=%D0%BF%D0%BE%D0%B1%D0%B5%D0%B3")
+
+let getVariableNames() =
+    fsiAssembly.GetTypes()//FSI types have the name pattern FSI_####, where #### is the order in which they were created
+    |> Seq.filter (fun ty -> ty.Name.StartsWith("FSI_"))
+    |> Seq.sortBy (fun ty -> ty.Name.Split('_').[1] |> int)
+    |> Seq.collect (fun ty ->
+        let flags = BindingFlags.Static ||| BindingFlags.NonPublic ||| BindingFlags.Public
+        ty.GetProperties(flags) 
+        |> Seq.filter (fun pi -> pi.GetIndexParameters().Length > 0  |> not && pi.Name.Contains("@") |> not))
+    |> Seq.map (fun pi -> pi.Name)
+    |> Seq.distinct
+
+getVariableNames() |> Seq.toList;;
+
+let getMethodNames() =
+    fsiAssembly.GetTypes()//FSI types have the name pattern FSI_####, where #### is the order in which they were created
+    |> Seq.filter (fun ty -> ty.Name.StartsWith("FSI_")) 
+    |> Seq.collect (fun ty ->
+        let flags = BindingFlags.Static ||| BindingFlags.NonPublic ||| BindingFlags.Public
+        ty.GetMethods(flags) 
+        |> Seq.filter (fun pi -> pi.Name.Contains("@") |> not && pi.Name.StartsWith("get_") |> not && pi.Name.StartsWith("set_") |> not))
+    |> Seq.map (fun pi -> pi.Name)
+    |> Seq.distinct
+    |> Seq.toList
+
+getMethodNames() |> Seq.toList;;
+
+
+
+let fsharpcore =  fsiAssembly.GetReferencedAssemblies() |> Seq.head
+
+fsiAssembly.GetReferencedAssemblies() |> Seq.map(fun a -> a.FullName) |> Seq.toList
+
+AppDomain.CurrentDomain.GetAssemblies() 
+                       |> Seq.filter(fun a -> a.GetName().FullName = fsharpcore.FullName) 
+                       |> Seq.toList
+
+let getCompletionsForTypes(statement:String) : IEnumerable<String> = seq {
+    for assemblyName in fsiAssembly.GetReferencedAssemblies() do
+        //printfn "%s" assemblyName.FullName
+        let matches = AppDomain.CurrentDomain.GetAssemblies() 
+                       |> Seq.filter(fun a -> a.GetName().FullName = assemblyName.FullName)
+        if matches |> Seq.isEmpty |> not then
+            let assembly = matches |> Seq.head
+            let assemblyTypeNames = getTypeCompletionsForAssembly(statement, assembly) 
+            yield! assemblyTypeNames
+
+            if statement.LastIndexOf('.') > 0 then
+                let type_ = assembly.GetType(statement.Remove(statement.LastIndexOf('.')))
+                if not(type_ = null) then
+                    let memberNames = type_.GetMembers(BindingFlags.Instance ||| BindingFlags.Static ||| BindingFlags.FlattenHierarchy ||| BindingFlags.Public)
+                                        |> Seq.map(fun m -> removePropertyPrefix m.Name)
+                                        |> Seq.distinct
+                    yield! memberNames
+}
+
 let getCompletions(statement:String) : IEnumerable<String> =
     seq {
-        let systemAssembly = Assembly.GetAssembly(typeof<Object>)
-        let systemAssemblyTypeNames = getTypeCompletionsForAssembly(statement, systemAssembly) 
-                                            
-        yield! systemAssemblyTypeNames
+        yield! getCompletionsForTypes(statement)
 
-        if statement.LastIndexOf('.') > 0 then
-            let type_ = systemAssembly.GetType(statement.Remove(statement.LastIndexOf('.')))
-            if not(type_ = null) then
-                let memberNames = type_.GetMembers(BindingFlags.Instance ||| BindingFlags.Static ||| BindingFlags.FlattenHierarchy ||| BindingFlags.Public)
-                                    |> Seq.map(fun m -> removePropertyPrefix m.Name)
-                                    |> Seq.distinct
-                yield! memberNames
-        
-    }
+        if statement.LastIndexOf('.') < 0 then
+            yield! getVariableNames() |> Seq.filter(fun n -> n.StartsWith(statement))
+            yield! getMethodNames() |> Seq.filter(fun n -> n.StartsWith(statement))
+    } 
+    |> Seq.distinct
+
+getCompletions("f") |> Seq.toList
 
 
 

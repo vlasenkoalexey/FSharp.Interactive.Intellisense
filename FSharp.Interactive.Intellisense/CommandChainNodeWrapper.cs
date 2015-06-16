4608e0b90f0de7e33938ec7c172ca3a6d156196f
Aleksey Vlasenko <vlasenkoalexey@gmail.com>
2015-06-15 17:59:50 -0700

    Code cleanup

---
+++ FSharp.Interactive.Intellisense/CommandChainNodeWrapper.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace FSharp.Interactive.Intellisense
+{
+    internal class CommandChainNodeWrapper
+    {
+        private const int LevelDeepMax = 20;
+
+        public IOleCommandTarget FilterObject
+        {
+            get
+            {
+                return commandChainNode.FilterObject;
+            }
+        }
+
+        public CommandChainNodeWrapper Next
+        {
+            get
+            {
+                if (commandChainNode.Next != null)
+                {
+                    return new CommandChainNodeWrapper(commandChainNode.Next);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        private dynamic commandChainNode;
+
+        public CommandChainNodeWrapper(Object commandChainNode)
+        {
+            this.commandChainNode = ExposedObject.From(commandChainNode);
+        }
+
+        public CommandChainNodeWrapper(IVsTextView textAdapter)
+        {
+            FieldInfo commandChainFieldInfo = textAdapter.GetType().BaseType.GetField("_commandChain", BindingFlags.NonPublic | BindingFlags.Instance);
+            this.commandChainNode = new CommandChainNodeWrapper(commandChainFieldInfo.GetValue(textAdapter));
+        }
+
+        public static IOleCommandTarget GetFilterByFullClassName(CommandChainNodeWrapper commandChainWrapper, string className)
+        {
+            return GetFilterByFullClassName(commandChainWrapper, className, 0);
+        }
+
+        private static IOleCommandTarget GetFilterByFullClassName(CommandChainNodeWrapper commandChainWrapper, string className, int levelDeep)
+        {
+            if (commandChainWrapper.FilterObject != null && commandChainWrapper.FilterObject.GetType().FullName == className)
+            {
+                return commandChainWrapper.FilterObject as IOleCommandTarget;
+            }
+
+            if (levelDeep >= LevelDeepMax)
+            {
+                return null;
+            }
+
+            if (commandChainWrapper.Next != null)
+            {
+                try
+                {
+                    return GetFilterByFullClassName(commandChainWrapper.Next, className, ++levelDeep);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                    return null;
+                }
+
+            }
+
+            return null;
+        }
+    }
+
+}
--- FSharp.Interactive.Intellisense/FSharpCompletionCommandHandler.cs
+++ FSharp.Interactive.Intellisense/FSharpCompletionCommandHandler.cs
@@ -34,226 +34,19 @@ namespace FSharp.Interactive.Intellisense
 
             this.m_textView = textView;
             this.m_provider = fsharpCompletionHandlerProvider;
-
-            //IOleCommandTarget fsiViewFilter =
-            //    CommandChainNodeWrapper.GetFilterByFullClassName(new CommandChainNodeWrapper(textViewAdapter),
-            //        "Microsoft.VisualStudio.FSharp.Interactive.FsiViewFilter");
-
-
-            //if (fsiViewFilter != null)
-            //{
-            //    textViewAdapter.RemoveCommandFilter(fsiViewFilter);
-            //}
-
-            //IOleCommandTarget intellisenseFilter =
-            //    CommandChainNodeWrapper.GetFilterByFullClassName(new CommandChainNodeWrapper(textViewAdapter),
-            //        "Microsoft.VisualStudio.Editor.Implementation.IntellisenseCommandFilter");
-
-            //if (intellisenseFilter != null)
-            //{
-            //    textViewAdapter.RemoveCommandFilter(intellisenseFilter);
-            //}
-
-            //add the command to the command chain
-
-            //Task.Delay(1000).ContinueWith((a) =>
-            //    {
-                    //IOleCommandTarget fsiToolWindow = CommandChainNodeWrapper.GetFilterByFullClassName(new CommandChainNodeWrapper(textViewAdapter), "Microsoft.VisualStudio.FSharp.Interactive.FsiToolWindow");
-                    //textViewAdapter.RemoveCommandFilter(fsiToolWindow);
-                    //IOleCommandTarget m_nextCommandHandler1;
-                    textViewAdapter.AddCommandFilter(this, out m_nextCommandHandler);
-                    //textViewAdapter.AddCommandFilter(fsiToolWindow, out m_nextCommandHandler);
-                    RegisterKeyDown(textView);
-                //});
-
+            textViewAdapter.AddCommandFilter(this, out m_nextCommandHandler);
 
             Task.Delay(2000).ContinueWith((a) =>
             {
-                // TODO: make it method safe by limiting max depth
                 this.fsiToolWindow = CommandChainNodeWrapper.GetFilterByFullClassName(new CommandChainNodeWrapper(textViewAdapter), "Microsoft.VisualStudio.FSharp.Interactive.FsiToolWindow");
             });
 
-            //textViewAdapter.RemoveCommandFilter()
-
             this.textViewAdapter = textViewAdapter;
-            //((System.Windows.Controls.Control)this.m_textView).AddHandler(.KeyDown += (h, e) =>
-            //    {
-            //        if (e.Key == System.Windows.Input.Key.Down)
-            //        {
-            //            e.Handled = true;
-            //        }
-            //    };
-
-
-            //((System.Windows.Controls.ContentControl)(((System.Windows.Controls.Grid)(((System.Windows.Controls.Control)(sender)).Parent)).Parent))
-            ((System.Windows.Controls.Control)this.m_textView).AddHandler(System.Windows.Window.PreviewKeyDownEvent, 
-                new KeyEventHandler(ControlViewer_KeyUp), true);
-
-            // TODO: try out to init built in intellisense provider again.
-
-            // TODO: figure out what exception is thrown when we are removing FsiToolWindow + 
-            // TODO: try to trick FsiToolWindow by setting isCurrentPositionInInputArea to false -
-            // TODO: try to trick FsiToolWindow by setting source.IsCompletorActive <- completionSet.IsDisplayed +
-            // TODO: try to attach key up/down event to another control - filter works, but event subscription doesnt
-            // TODO: read about adding up / down events to wpf textbox
-            // TODO: try attaching to the parent element (see above)
-            // TODO: look in F# intellisense code, how pagedown is handled +++
-            // TODO: test that select next/previous apprach works
-            // TODO: copy custom visual provider from https://github.com/techtalk/SpecFlow.VisualStudio/blob/master/VsIntegration/AutoComplete/IntellisensePresenter/CompletionSessionPresenter.cs
-        
-            // TODO: use reflection to find FsiFilter in command chain 
-            //((Microsoft.VisualStudio.Editor.Implementation.CommandChainNode)(((Microsoft.VisualStudio.Editor.Implementation.CommandChainNode)(((Microsoft.VisualStudio.Editor.Implementation.CommandChainNode)(((Microsoft.VisualStudio.Editor.Implementation.SimpleTextViewWindow)(textViewAdapter))._commandChain.Next)).Next)).Next))
-            // Remove it and add only after my filer
-            // ((Microsoft.VisualStudio.Package.Source)(((Microsoft.VisualStudio.FSharp.Interactive.FsiToolWindow)(fsiToolWindow)).source)).CompletionSet - is it intellisense?
-
         }
 
-        private class CommandChainNodeWrapper
-        {
-            public IOleCommandTarget FilterObject
-            {
-                get
-                {
-                    return commandChainNode.FilterObject;
-                }
-            }
-
-            public CommandChainNodeWrapper Next
-            {
-                get
-                {
-                    if (commandChainNode.Next != null)
-                    {
-                        return new CommandChainNodeWrapper(commandChainNode.Next);
-                    }
-                    else
-                    {
-                        return null;
-                    }
-                }
-            }
-
-            private dynamic commandChainNode;
-
-            public CommandChainNodeWrapper(Object commandChainNode)
-            {
-                this.commandChainNode = ExposedObject.From(commandChainNode);
-            }
-
-            public CommandChainNodeWrapper(IVsTextView textAdapter)
-            {
-                FieldInfo commandChainFieldInfo = textAdapter.GetType().BaseType.GetField("_commandChain", BindingFlags.NonPublic | BindingFlags.Instance);
-                this.commandChainNode = new CommandChainNodeWrapper(commandChainFieldInfo.GetValue(textAdapter));
-            }
-
-            public static IOleCommandTarget GetFilterByFullClassName(CommandChainNodeWrapper commandChainWrapper, string className)
-            {
-                if (commandChainWrapper.FilterObject != null && commandChainWrapper.FilterObject.GetType().FullName == className)
-                {
-                    return commandChainWrapper.FilterObject as IOleCommandTarget;
-                }
-
-                if (commandChainWrapper.Next != null)
-                {
-                    try
-                    {
-                        return GetFilterByFullClassName(commandChainWrapper.Next, className);
-                    } catch (Exception ex)
-                    {
-                        return null;
-                    }
-                   
-                }
-
-                return null;
-            }
-        }
-
-
-
-        private void MenuCommandCallback(object sender, EventArgs e)
-        {
-            System.Windows.Forms.MessageBox.Show(sender.ToString());
-        }
-
-        private void RegisterKeyDown(ITextView textView)
-        {
-            OleMenuCommandService oleMenuCommandService;
-            CommandID commandID;
-            OleMenuCommand menuCommand;
-            
-            //var providerGlobal = (IOleServiceProvider)Package.GetGlobalService(typeof(IOleServiceProvider));
-            
-            //var provider = new ServiceProvider(providerGlobal);
-
-            //Package package = m_provider.GetPackage();
-            //OleMenuCommandService oleMenuCommandService = ExposedObject.From(package).GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-
-            //Assembly fsiAssembly = Assembly.Load("FSharp.VS.FSI, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
-            //Type fsiToolWindowType = fsiAssembly.GetType("Microsoft.VisualStudio.FSharp.Interactive.FsiToolWindow");
-
-            //IVsOutputWindowPane fsiToolWindowPane = package.GetOutputPane(new Guid("dee22b65-9761-4a26-8fb2-759b971d6dfc"), "F# Interactive");
-
-           
-
-            //oleMenuCommandService = provider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            //dynamic textViewAdapterExposedObject = ExposedObject.From(textViewAdapter);
-            //oleMenuCommandService = textViewAdapterExposedObject.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-
-            //IOleCommandTarget fsiToolWindow = CommandChainNodeWrapper.GetFilterByFullClassName(new CommandChainNodeWrapper(textViewAdapter), "Microsoft.VisualStudio.FSharp.Interactive.FsiToolWindow");
-
-            ////this.textViewAdapter.RemoveCommandFilter(fsiToolWindow);
-            
-            //System.IServiceProvider provider = (System.IServiceProvider)(Object)fsiToolWindow;
-
-            //if (provider == null)
-            //{
-            //    return;
-            //}
-
-            //oleMenuCommandService = ExposedObject.From(provider).commandService;
-                
-            oleMenuCommandService = new OleMenuCommandService((System.IServiceProvider)(Object)textViewAdapter, m_nextCommandHandler);
-
-            if (oleMenuCommandService != null)
-            {
-                var guidVSStd2KCmdID = typeof(VSConstants.VSStd2KCmdID).GUID;
-                commandID = new CommandID(guidVSStd2KCmdID, (int)VSConstants.VSStd2KCmdID.BACKSPACE);
-
-                menuCommand = new OleMenuCommand(MenuCommandCallback, commandID);
-                menuCommand.BeforeQueryStatus += menuCommand_BeforeQueryStatus;
-
-                //oleMenuCommandService.AddCommand(menuCommand);
-            }
-        }
-
-        private void MoveFsiWindowFilterAfterSelf()
-        {
-
-            //this.textViewAdapter.RemoveCommandFilter(fsiToolWindow);
-
-        }
-
-        void menuCommand_BeforeQueryStatus(object sender, EventArgs e)
-        {
-
-        }
-
-        private void ControlViewer_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.Key == System.Windows.Input.Key.Tab || e.Key == Key.Space || e.Key == Key.Up)
-            {
-            //    e.Handled = true;
-            }
-        }
 
         public int QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
         {
-            //if (pguidCmdGroup == VSConstants.VSStd2K)
-            //{
-            //    Debug.WriteLine(String.Format("QueryStatus up[{0}] down[{1}] value[{2}]", (uint)VSConstants.VSStd2KCmdID.UP,
-            //        (uint)VSConstants.VSStd2KCmdID.DOWN, ((prgCmds != null && prgCmds.Length > 0) ? prgCmds[0].cmdID.ToString() : "")));
-            //}
             return m_nextCommandHandler.QueryStatus(ref pguidCmdGroup, cCmds, prgCmds, pCmdText);
         }
 
@@ -262,12 +55,6 @@ namespace FSharp.Interactive.Intellisense
             IOleCommandTarget fsiToolWindowFilter =
                 CommandChainNodeWrapper.GetFilterByFullClassName(new CommandChainNodeWrapper(textViewAdapter),
                     "Microsoft.VisualStudio.FSharp.Interactive.FsiToolWindow");
-
-            //textViewAdapter.RemoveCommandFilter(fsiToolWindowFilter);
-
-
-            Debug.WriteLine(String.Format("Exec up[{0}] down[{1}] value[{2}]", (uint)VSConstants.VSStd2KCmdID.UP,
-                (uint)VSConstants.VSStd2KCmdID.DOWN, nCmdID.ToString()));
 
             if (VsShellUtilities.IsInAutomationFunction(m_provider.ServiceProvider))
             {
@@ -281,11 +68,6 @@ namespace FSharp.Interactive.Intellisense
             {
                 typedChar = (char)(ushort)Marshal.GetObjectForNativeVariant(pvaIn);
             }
-
-            //if (m_session != null && !m_session.IsDismissed && (nCmdID == (uint)VSConstants.VSStd2KCmdID.DOWN || nCmdID == (uint)VSConstants.VSStd2KCmdID.UP))
-            //{
-            //    return VSConstants.S_OK;
-            //}
 
             //check for a commit character 
             if (nCmdID == (uint)VSConstants.VSStd2KCmdID.RETURN
@@ -347,26 +129,11 @@ namespace FSharp.Interactive.Intellisense
 
                 if (m_session != null && m_session.Presenter != null)
                 {
+                    handled = true;
                     dynamic presenter = ExposedObject.From(m_session.Presenter);
                     IIntellisensePresenter intelisensePresenter = m_session.Presenter;
                     System.Windows.Controls.ContentControl surfaceElement = (System.Windows.Controls.ContentControl)presenter.SurfaceElement;
-                    surfaceElement.KeyDown += (h, e) =>
-                    {
-                        if (e.Key == System.Windows.Input.Key.Down)
-                        {
-                            e.Handled = true;
-                        }
-                    };
-                    surfaceElement.KeyUp += (h, e) =>
-                    {
-                        if (e.Key == System.Windows.Input.Key.Down)
-                        {
-                            e.Handled = true;
-                        }
-                    };
                     surfaceElement.Focus();
-
-                    handled = true;
                 }
             }
             else if (commandID == (uint)VSConstants.VSStd2KCmdID.BACKSPACE   //redo the filter if there is a deletion
@@ -391,9 +158,6 @@ namespace FSharp.Interactive.Intellisense
                 return false;
             }
 
-            //m_session = m_provider.CompletionBroker.CreateCompletionSession(m_textView,
-            //    caretPoint.Value.Snapshot.CreateTrackingPoint(caretPoint.Value.Position, PointTrackingMode.Positive),
-            //    true);
             m_session = m_provider.CompletionBroker.TriggerCompletion(m_textView);
 
             if (m_session == null)
@@ -403,25 +167,6 @@ namespace FSharp.Interactive.Intellisense
 
             //subscribe to the Dismissed event on the session 
             m_session.Dismissed += this.OnSessionDismissed;
-
-            //HACK start
-            //var snapshot = new SnapshotPoint(m_textView.TextBuffer.CurrentSnapshot, m_textView.Caret.Position.BufferPosition.Position - 1);
-            //m_textView.TextBuffer.Insert(m_textView.TextBuffer.CurrentSnapshot.Length, " ");
-            //m_textView.Caret.MoveTo(m_textView.Caret.ContainingTextViewLine, m_textView.Caret.Left - 10);
-            //HACK end
-
-            //HACK start
-            //var snapshot = new SnapshotPoint(m_textView.TextBuffer.CurrentSnapshot, m_textView.Caret.Position.BufferPosition.Position);
-            //m_textView.TextBuffer.Insert(m_textView.TextBuffer.CurrentSnapshot.Length, " ");
-            //m_textView.Caret.MoveTo(caretPoint.Value);
-            //HACK end
-
-            //var pos = new SnapshotPoint(m_textView.Caret.Position.BufferPosition.Snapshot,
-            //                m_textView.Caret.Position.BufferPosition.Position);
-            //m_textView.TextBuffer.Insert(m_textView.TextBuffer.CurrentSnapshot.Length, " ");
-            //m_textView.Caret.MoveTo(pos);
-
-            //m_session.Start();
 
             // TODO: wrap fsiToolWindow into class
             if (fsiToolWindow != null && m_session != null)

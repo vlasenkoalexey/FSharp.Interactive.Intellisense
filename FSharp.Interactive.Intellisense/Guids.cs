63bf624b6e84039930215ac0c123c10ea8d710f4
Aleksey Vlasenko <vlasenkoalexey@gmail.com>
2015-09-02 12:42:31 -0700

    Added empty settings page

---
+++ FSharp.Interactive.Intellisense/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace FSharp.Interactive.Intellisense
+{
+    static class GuidList
+    {
+        public const string guidFSharp_Interactive_IntellisensePkgString = "225a4dcf-86ef-4279-b906-f4155df19e5f";
+        public const string guidFSharp_Interactive_IntellisenseCmdSetString = "18e0bce4-dfa9-4f39-a7bb-1998fdb3a0ee";
+
+        public static readonly Guid guidFSharp_Interactive_IntellisenseCmdSet = new Guid(guidFSharp_Interactive_IntellisenseCmdSetString);
+    };
+}
---
+++ FSharp.Interactive.Intellisense/OptionsPageGrid.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSharp.Interactive.Intellisense
+{
+    public class OptionsPageGrid : DialogPage
+    {
+        private int optionInt = 256;
+        public enum AutocompleteMode
+        {
+            Automatic,
+            CtrlSpace,
+            Off
+        }
+
+        public enum IntellisenseProvider
+        {
+            Combined,
+            FsiSession,
+            Internal
+        }
+
+        [Category("Settings")]
+        [DisplayName("Autocomplete mode")]
+        [Description("The way autocomplete is triggered")]
+        public int OptionInteger
+        {
+            get { return optionInt; }
+            set { optionInt = value; }
+        }
+    }
+}
